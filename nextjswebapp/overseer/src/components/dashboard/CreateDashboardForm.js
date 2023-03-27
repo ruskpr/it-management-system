2daca5e6c7043d4da3ae42425b8a5c03ec4dab63
Rustu Koprulu <rustulp@gmail.com>
2023-03-27 08:15:34 -0600

    updated to password field

--- nextjswebapp/overseer/src/components/dashboard/CreateDashboardForm.js
+++ nextjswebapp/overseer/src/components/dashboard/CreateDashboardForm.js
@@ -116,6 +116,7 @@ export default function CreateDashboardForm() {
             Access Key
           </label>
           <input
+          type="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             name="accessKey"
             onChange={handleChange}
@@ -127,6 +128,7 @@ export default function CreateDashboardForm() {
             Confirm Access Key
           </label>
           <input
+          type="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             name="confirmAccessKey"
             onChange={handleChange}
--- nextjswebapp/overseer/src/components/dashboard/LoginForm.js
+++ nextjswebapp/overseer/src/components/dashboard/LoginForm.js
@@ -99,6 +99,7 @@ export default function LoginForm() {
             Access Key
           </label>
           <input
+          type="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             name="accessKey"
             onChange={handleChange}
